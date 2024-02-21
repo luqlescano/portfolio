8aa060cf00c91ac7338552c0f43d1971a17ce6dc
Lucas <lucasnlescano@gmail.com>
2024-02-21 18:52:03 -0300

    Fix bug en el enlace del componente

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -5,7 +5,7 @@ const Contacto = () => {
             <div className="contacto-contenedor">
                 <p>Si querés agendar directamente una reunión virtual, podes hacerlo desde mi <a href="https://calendario.lucaslescano.com.ar" target="_blank">Calendario</a>.</p>
                 <p>Otra opción, para consultas, es enviarme un e-mail a <a href="mailto:hola@lucaslescano.com.ar" target="_blank">hola@lucaslescano.com.ar</a></p>
-                <p>Ó escribime a través de <a href="https:whatsapp.lucaslescano.com.ar" target="_blank">WhatsApp</a>.</p>
+                <p>Ó escribime a través de <a href="https://whatsapp.lucaslescano.com.ar" target="_blank">WhatsApp</a>.</p>
             </div>
             <p className="finSubTitulo">Contacto</p>
         </section>
