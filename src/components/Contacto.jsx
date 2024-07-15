5b2487b5fc7fa61500de8fb7f95ec60ec2945c3a
Lucas <lucasnlescano@gmail.com>
2024-07-15 19:45:38 -0300

    Se actualizó el portfolio y los enlaces de contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -3,9 +3,9 @@ const Contacto = () => {
         <section id="contacto" className="contacto">
             <h2 className="comienzoSubTitulo">Contacto</h2>
             <div className="contacto-contenedor">
-                <p>Si querés agendar directamente una reunión virtual, podes hacerlo desde mi <a href="https://calendario.lucaslescano.com.ar" target="_blank">Calendario</a>.</p>
-                <p>Otra opción, para consultas, es enviarme un e-mail a <a href="mailto:hola@lucaslescano.com.ar" target="_blank">hola@lucaslescano.com.ar</a></p>
-                <p>Ó escribime a través de <a href="https://whatsapp.lucaslescano.com.ar" target="_blank">WhatsApp</a>.</p>
+                <p>Si querés agendar directamente una reunión virtual, podes hacerlo desde mi <a href="https://calendario.lucaslescano.dev" target="_blank">Calendario</a>.</p>
+                <p>Otra opción, para consultas, es enviarme un e-mail a <a href="mailto:hola@lucaslescano.dev" target="_blank">hola@lucaslescano.dev</a></p>
+                <p>Ó escribime a través de <a href="https://whatsapp.lucaslescano.dev" target="_blank">WhatsApp</a>.</p>
             </div>
             <p className="finSubTitulo">Contacto</p>
         </section>
